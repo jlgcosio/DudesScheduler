b08b6a306b910e7fa1ee1821386a2b3b01b60cf4
Jono Cosio <jonoluiz@gmail.com>
2021-01-31 09:58:51 +0800

    Edit test command for fields

--- commands/horde/post-embed.js
+++ commands/horde/post-embed.js
@@ -8,7 +8,7 @@ module.exports = class PostEmbedCommand extends Command {
 			name: 'post',
 			group: 'horde',
 			memberName: 'embed',
-            description: 'Posts a specific embed message',
+            description: 'Command for testing hard-coded embeds',
             args: [
                 {
                     'key': 'title',
@@ -31,15 +31,39 @@ module.exports = class PostEmbedCommand extends Command {
     }
 
     run(message, {title, description, image}){
+        // Set limit for sign ups (user input)
+        var limit = 10
+        // Get every user who reacted (separate listener class)
+        var fieldValues =["first", "second", "third"];
+        
+        // Create embed fields
+        var fieldTitle = "Players: " + (fieldValues.length) + "/" + limit;
+        var fields = [
+            {
+                name: fieldTitle,
+                value: fieldValues,
+                inline: true
+            },
+            {
+                name: "Field 2",
+                value: "Field 2 value",
+                inline: true
+            },
+            {
+                name: "Field 3",
+                value: "Field 3 value",
+                inline: false
+            },
+        ];
+
         // Creates new MessageEmbed object
         const msg = new MessageEmbed()
             .setTitle(title)
             .setDescription(description)
-            .setImage(image);
+            .setImage(image)
+            .addFields(fields);
 
         console.log(image);
         return this.client.channels.cache.get('800659640362467340').send(msg);
     }
-
-    
 }
